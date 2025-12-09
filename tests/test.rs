d82110583519ca359e4494cedb51f392097bed9e
Jalon Wong <jalonwong@gmail.com>
2025-12-09 12:10:02 -0600

    Add test data

--- tests/test.rs
+++ tests/test.rs
@@ -8,18 +8,24 @@ fn test_sync_code() {
         // Chain dependence
         .add("tests/data3/source2.txt", "tests/data3/source.txt")
         .add("tests/data3/target.txt", "tests/data3/source2.txt")
+        .add("tests/data4_lf/source2.txt", "tests/data4_lf/source.txt")
+        .add("tests/data4_lf/target.txt", "tests/data4_lf/source2.txt")
         .sync();
 
     assert_eq!(
-        fs::read_to_string("tests/data1/target.txt").unwrap(),
-        fs::read_to_string("tests/data1/source.txt").unwrap()
+        fs::read("tests/data1/target.txt").unwrap(),
+        fs::read("tests/data1/source.txt").unwrap()
     );
     assert_eq!(
-        fs::read_to_string("tests/data2/target.txt").unwrap(),
-        fs::read_to_string("tests/data2/expected.txt").unwrap()
+        fs::read("tests/data2/target.txt").unwrap(),
+        fs::read("tests/data2/expected.txt").unwrap()
     );
     assert_eq!(
-        fs::read_to_string("tests/data3/target.txt").unwrap(),
-        fs::read_to_string("tests/data3/expected.txt").unwrap()
+        fs::read("tests/data3/target.txt").unwrap(),
+        fs::read("tests/data3/expected.txt").unwrap()
+    );
+    assert_eq!(
+        fs::read("tests/data4_lf/target.txt").unwrap(),
+        fs::read("tests/data4_lf/expected.txt").unwrap()
     );
 }
